b96de1f09a053887da03035c88aca8e4394be1d5
vinodbahiram@24 <vinodbahiram@gmail.com>
2024-09-06 15:28:06 +0530

    6

--- frontend/src/Components/Pages/SignInPage.js
+++ frontend/src/Components/Pages/SignInPage.js
@@ -51,32 +51,31 @@ export default function SignInPage(props) {
   setTimeout(() => {
     setSignUpMessage("");
     setSignInMessage("");
-  }, 10000); 
+  }, 5000); 
   
   return (
-    <div className="container">
-      <div className="row py-5 ">
+    <div className="login">
+      <div className="row">
         {/* sign up */}
         <div className="signUp col-md-4">
-          <form>
-            <label aria-hidden="true">Sign Up</label>
+          <form onSubmit={handleSignUp}>
+            <label>Create a New Account</label>
             <input id="username" value={signUpUsername} type="text" placeholder=" UserName" onChange={(e)=>setSignUpUsername(e.target.value)} required></input>
             <input id="email" value={signUpEmail} type="email" placeholder=" Email" onChange={(e)=>setSignUpEmail(e.target.value)} required></input>
             <input id="mobile" value={signUpMobile} type="tel" placeholder=" Mobile" onChange={(e)=>setSignUpMobile(e.target.value)} required></input>
             <input id="password" value={signUpPassword} type="password" placeholder=" Password" onChange={(e)=>setSignUpPassword(e.target.value)} required></input>
-            <button type="submit" onClick={handleSignUp}>Sign Up</button>
-            {signUpMessage && (<div className="message-div"><p>{signUpMessage}</p></div>
-          )}
+            <button type="submit">Create</button>
+            <div className="message-div"><p>{signUpMessage}</p></div>
           </form>
         </div>
         {/* sign in */}
         <div className="signIn col-md-4">
-          <form>
-            <label aria-hidden="true">Sign In</label>
+          <form onSubmit={handleSignIn}> 
+            <label>Or Login to Existing One</label>
             <input id="signInUsername" value={signInUsername} type="text" placeholder=" UserName" onChange={(e)=>setSignInUsername(e.target.value)} required></input>
             <input id="signInPassword" value={signInPassword} type="password" placeholder=" Password" onChange={(e)=>setSignInPassword(e.target.value)} required></input>
-            <button type="submit" onClick={handleSignIn}>Sign In</button>
-            <div><p style={{color:'white'}}>{signInMessage}</p></div>
+            <button type="submit">Login</button>
+            <div className="message-div"><p>{signInMessage}</p></div>
           </form>
         </div>
       </div>
